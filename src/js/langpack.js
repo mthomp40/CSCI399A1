1eb7c564eb4f318277c6f3aaa314a2fb2c47b3a1
mthomp40 <mthommo79@gmail.com>
2015-03-20 19:47:37 +1100

    Adding

---
+++ src/js/langpack.js
@@ -0,0 +1,10 @@
+Lang.prototype.pack.fr = {
+    "token": {
+        "Title": "Loi et l'ordre",
+    "Law & Order: Special Victims Unit": "this is the french"
+    },
+    "regex": [
+        [/^Something at start of text/, ""],
+        [/This will be case insensitive/i, ""]
+    ]
+};
